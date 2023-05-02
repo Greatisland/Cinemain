5e257d3dc412975dfeb4c147674c99050de744c0
Hyeon JIN <dontsendloveletters@gmail.com>
2023-05-02 00:07:30 +0000

    Updates

--- src/controllers/hooks.ts
+++
@@ -1,6 +0,0 @@
-import { TypedUseSelectorHook } from "react-redux";
-import { useSelector, useDispatch } from "react-redux";
-import { RootState, AppDispatch } from "./configureStore";
-
-export const useAppDispatch: () => AppDispatch = useDispatch
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
--- src/pages/sub/SubPages.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react"
-interface SubPagesProps {
-  children: ReactNode
-}
-const SubPages = ({children}: SubPagesProps) => (
-  <>{children}</>
-)
-
-export default SubPages
